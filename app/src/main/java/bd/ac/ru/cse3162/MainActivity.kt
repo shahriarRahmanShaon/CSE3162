c8cbbe137916bf080cf8ccd8752eb9a53f54e273
shahriarRahmanShaon <shaon463@gmail.com>
2021-12-23 12:56:44 +0600

    taskNo1

--- app/src/main/java/bd/ac/ru/cse3162/MainActivity.kt
+++ app/src/main/java/bd/ac/ru/cse3162/MainActivity.kt
@@ -2,11 +2,34 @@ package bd.ac.ru.cse3162
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var search: Button
+    lateinit var lat: TextView
+    lateinit var lon: TextView
+    lateinit var viewWeather: Button
+    lateinit var city_date: TextView
+    lateinit var celciousView: TextView
+    lateinit var farenhiteView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // connecting them with this activity
+        search = findViewById(R.id.bSearch)
+        lat = findViewById(R.id.lat)
+        lon = findViewById(R.id.lon)
+        viewWeather = findViewById(R.id.weatherBtn)
+        city_date = findViewById(R.id.cityAndName)
+        celciousView = findViewById(R.id.celciusTemp)
+        farenhiteView = findViewById(R.id.farenTemp)
+
+        // initializing button press for Location Button
+        
+
+
     }
 }
